c9ed82dc7f25c18bd0265bb8b33183d994a0645f
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-11 14:04:28 +0100

    added nodemon to auto build after save

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) => res.send('Hello World'))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+res.send('Hello World')
+})
+
+app.get('/todo', (req, res) => {
+    res.send('Hello World')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
